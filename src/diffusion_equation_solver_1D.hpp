a0607a61f7f8840798e680f09506eda25f61bfb3
Eivind Støland <eivinsto@student.matnat.uio.no>
2020-12-10 14:06:42 +0100

    Added comments and some documentation to 1D solver's header

--- src/diffusion_equation_solver_1D.hpp
+++ src/diffusion_equation_solver_1D.hpp
@@ -3,20 +3,29 @@
 #include <string>
 #include <fstream>
 
+/**
+* Class that is used to solve a dimensionless 1D diffusion equation with either
+* a forward Euler based explicit scheme, a backward Euler based implicit scheme,
+* or the Crank-Nicolson scheme.
+*/
 class DiffusionEquationSolver1D{
 public:
+  // Constructor
   DiffusionEquationSolver1D(int, double, int, int, double(*)(double), std::string,
                           std::string, double, double);
+  // Public functions
   void solve();
 
 private:
+  // Private functions
   void forward_euler_solve();
   void backward_euler_solve();
   void crank_nicholson_solve();
   void tridiag();
-  double (*m_init_func)(double);
   void write_to_file();
+  double (*m_init_func)(double); // Function pointer used to generate initial state of system 
 
+  // Private variables
   int m_N;
   arma::vec m_u;
   double m_dx;
