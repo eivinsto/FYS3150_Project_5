0c76fd14318a869e10f614b0ef7ed8d4fced4c0d
Anders Petershagen Åsbø <aanders35@gmail.com>
2020-12-14 15:04:53 +0100

    Adjusted load balancing. 2D solver uses 2/100 * m_N threads

--- src/diffusion_equation_solver_2D.cpp
+++ src/diffusion_equation_solver_2D.cpp
@@ -108,10 +108,14 @@ void DiffusionEquationSolver2D::jacobi(){
 
   int k = 0;
   int i, j;
-  int thrds = 0.5*omp_get_max_threads();
+  int thrds = 0.75*omp_get_max_threads();
+  int thrds_to_use = std::min(3*m_N/100, thrds);
+  if (m_t == 1) {
+    std::cout << "Simulating 2D diffusion with " << thrds_to_use << " threads." << std::endl;
+  }
   // Iterative solver
   while (!converged && k < m_maxiter){
-    #pragma omp parallel num_threads(thrds) if(m_N > 100) default(shared) private(i, j) firstprivate(m_diag_element, m_alpha, m_Ax, m_Ay)
+    #pragma omp parallel num_threads(thrds_to_use) if(m_N > 100) default(shared) private(i, j) firstprivate(m_diag_element, m_alpha, m_Ax, m_Ay)
     {
       // Define elements used for extraction
       double u10;
